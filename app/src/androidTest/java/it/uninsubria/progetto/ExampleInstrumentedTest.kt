2838afb8f078ce61850fa02c502cdeffba505b5e
Leonardo <millo.leonardo99@gmail.com>
2023-06-17 16:01:27 +0200

    F

--- app/src/androidTest/java/it/uninsubria/progetto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/it/uninsubria/progetto/ExampleInstrumentedTest.kt
@@ -1,8 +1,13 @@
 package it.uninsubria.progetto
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
 
+/*import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+*/
+ */
 import org.junit.Test
 import org.junit.runner.RunWith
 
